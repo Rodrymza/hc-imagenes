c33bd927211ee595f39d29f0d9045a3351318fa9
rodry <rodrymza@gmail.com>
2026-01-01 17:29:08 -0300

    creacion de servicio para conexion con la api de internacion

--- src/modules/internacion/internacion.api.service.ts
+++ src/modules/internacion/internacion.api.service.ts
@@ -1,8 +1,23 @@
+import { internacionApi } from "./internacion.api";
+import { getInternacionToken } from "./internacion.auth";
+import { mapearPedidoInternacion } from "./internacion.mapper";
 import { InternacionService } from "./internacion.service";
 
 export const apiInternacionService: InternacionService = {
-  async obtenerPedidos() {
-    // llamada real a la API hospital
-    return [];
+  async obtenerPedidos(fecha?: string) {
+    const token = await getInternacionToken();
+
+    const res = await internacionApi.get("/hclinica/movimientos/tipo", {
+      params: {
+        fecha_desde: fecha,
+        fecha_hasta: fecha,
+        tipo: "ficha_solicitud_estudios_dimagenes",
+      },
+      headers: {
+        Cookie: `token=${token}`,
+      },
+    });
+
+    return res.data.map(mapearPedidoInternacion);
   },
 };

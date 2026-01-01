ab5f950747376b046e7b896850af6c4654554c25
rodry <rodrymza@gmail.com>
2026-01-01 17:30:43 -0300

    limpieza y manejo de logica en controlador

--- src/modules/internacion/internacion.routes.ts
+++ src/modules/internacion/internacion.routes.ts
@@ -1,22 +1,7 @@
 import { Router } from "express";
-import { mockInternacionService } from "./internacion.mock.service";
-import { apiInternacionService } from "./internacion.api.service";
+import { internacionController } from "./internacion.controller";
 
 const internacionRoutes = Router();
-console.log("USE_MOCK_API:", process.env.USE_MOCK_API);
 
-const service =
-  process.env.USE_MOCK_API === "true"
-    ? mockInternacionService
-    : apiInternacionService;
-
-internacionRoutes.get("/", async (_req, res, next) => {
-  try {
-    const data = await service.obtenerPedidos();
-    res.json(data);
-  } catch (e) {
-    next(e);
-  }
-});
-
+internacionRoutes.get("/", internacionController.obtenerPedidosInternacion);
 export default internacionRoutes;

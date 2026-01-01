704be7d206a1cdfd5d9484e64c8472d9a53a98b9
rodry <rodrymza@gmail.com>
2026-01-01 16:42:53 -0300

    aliminada importacion innecesaria

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler";
-import { AppError } from "./errors/AppError";
 import router from "./routes/routes";
 
 const app = express();

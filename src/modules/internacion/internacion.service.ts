d8456da290b79c58ca3291dd1982700292f5b060
rodry <rodrymza@gmail.com>
2026-01-16 01:15:35 -0300

    agregada funcion de guardar comentario a servicio internacion

--- src/modules/internacion/internacion.service.ts
+++ src/modules/internacion/internacion.service.ts
@@ -1,5 +1,12 @@
+import { IServiceResponse } from "../../types/common.types";
 import { IPedidoInternacion } from "./internacion.types";
 
 export interface InternacionService {
   obtenerPedidos(fecha?: string): Promise<IPedidoInternacion[]>;
+  guardarComentario(
+    idEstudio: string,
+    idMovimiento: string,
+    comentario: string,
+    nota: string
+  ): Promise<IServiceResponse<any>>;
 }

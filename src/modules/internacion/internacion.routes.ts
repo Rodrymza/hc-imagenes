6613ab9049e26e88d5813bbdb96330d17ed7621a
rodry <rodrymza@gmail.com>
2026-01-16 01:15:15 -0300

    agregada ruta de comentario

--- src/modules/internacion/internacion.routes.ts
+++ src/modules/internacion/internacion.routes.ts
@@ -4,4 +4,6 @@ import { internacionController } from "./internacion.controller";
 const internacionRoutes = Router();
 
 internacionRoutes.get("/", internacionController.obtenerPedidosInternacion);
+internacionRoutes.post("/comentario", internacionController.guardarComentario);
+
 export default internacionRoutes;

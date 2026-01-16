56ca9a457a30bc033f1135b28e98840514866fa4
rodry <rodrymza@gmail.com>
2026-01-16 01:14:52 -0300

    agregado mock de guardar comentario

--- src/modules/internacion/internacion.mock.service.ts
+++ src/modules/internacion/internacion.mock.service.ts
@@ -1,10 +1,16 @@
-import estudios from "../../mocks/internacion.api.json" with {type: "json"}
+import estudios from "../../mocks/internacion.api.json" with {type: "json"};
 import { mapearPedidoInternacion } from "./internacion.mapper";
 import { InternacionService } from "./internacion.service";
-
 
 export const mockInternacionService: InternacionService = {
   async obtenerPedidos(fecha?: string) {
     return estudios.map(mapearPedidoInternacion);
+  },
+  guardarComentario(idEstudio, idMovimiento, comentario, nota): any {
+    return {
+      success: true,
+      message: "Comentario guardado correctamente (modo mock)",
+      data: { Comentario: comentario, Nota: nota },
+    };
   },
 };

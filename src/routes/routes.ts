0a0e969e6f2895f2076751f53c790426f2ce244a
rodry <rodrymza@gmail.com>
2026-01-21 11:47:06 -0300

    agregadas rutas de sistema interno y login general

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,9 @@ import { Router } from "express";
 import { AppError } from "../errors/AppError";
 import internacionRoutes from "../modules/internacion/internacion.routes";
 import guardiaRoutes from "../modules/guardia/guardia.routes";
+import internoRoutes from "../modules/interno/interno.routes";
+import authRoutes from "../modules/auth/auth.routes";
+import { protegerRuta } from "../modules/auth/auth.middleware";
 
 const router = Router();
 
@@ -14,7 +17,8 @@ router.get("/error-test", () => {
   throw new AppError("Error de prueba", 400, "Detalle de prueba");
 });
 
-router.use("/internacion", internacionRoutes);
-router.use("/guardia", guardiaRoutes);
-
+router.use("/internacion", protegerRuta, internacionRoutes);
+router.use("/guardia", protegerRuta, guardiaRoutes);
+router.use("/interno", protegerRuta, internoRoutes);
+router.use("/auth", authRoutes);
 export default router;

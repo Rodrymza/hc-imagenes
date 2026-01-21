dd291bf522a55c31c1a50e59a861b3c41687bfea
rodry <rodrymza@gmail.com>
2026-01-21 11:40:40 -0300

    agregado manejo con cookieParser

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import { errorHandler } from "./middlewares/errorHandler";
 import router from "./routes/routes";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 // Middlewares básicos
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser()); // <--- Habilita req.cookies
 
 app.use("/api", router);
 

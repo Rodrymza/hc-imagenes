b4aa8444ab039cf56611ccc0f363cc3762c01e6b
rodry <rodrymza@gmail.com>
2026-03-06 13:33:25 -0300

    refactor: set base URL and enable credentials for axios in guardia and internacion services

--- client/src/services/internacion.service.ts
+++ client/src/services/internacion.service.ts
@@ -1,6 +1,16 @@
 import type { IEnvioComentario, IPedidoInternacion } from "@/types/pedidos";
 import axios from "axios";
 
+const isProd = import.meta.env.PROD;
+
+// 1. Definir la URL base correctamente
+axios.defaults.baseURL = isProd
+  ? `http://${window.location.hostname}:3000` // Producción (IP del server)
+  : "http://localhost:3000"; // Desarrollo (Tu PC local)
+
+// 2. IMPORTANTE: Activar credenciales
+axios.defaults.withCredentials = true;
+
 export const InternacionService = {
   getPedidos: async (fecha?: string): Promise<IPedidoInternacion[]> => {
     const url = fecha

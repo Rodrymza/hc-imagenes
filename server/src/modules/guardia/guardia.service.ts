400503e5248e15b016126916334e6c996fe936b2
rodry <rodrymza@gmail.com>
2026-07-14 23:07:03 -0300

    feat: agregado userid en request

--- server/src/modules/guardia/guardia.service.ts
+++ server/src/modules/guardia/guardia.service.ts
@@ -2,12 +2,25 @@ import {
   IDatosPacienteGuardia,
   IDetallePedidoGuardia,
   IPedidoGuardia,
-} from "./guardia.types";
+} from "./guardia.types.js";
 
 export interface GuardiaService {
-  obtenerPedidosGuardia(fecha?: string): Promise<IPedidoGuardia[]>;
-  obtenerPedidosPaciente(idPaciente: string): Promise<IDetallePedidoGuardia[]>;
-  finalizarPedido(idEstudio: string, idPatient: string): Promise<string>;
-  transferirPedido(idEstudio: string): Promise<string>;
-  buscarDatosPacienteGuardia(dni: string): Promise<IDatosPacienteGuardia>;
+  obtenerPedidosGuardia(
+    userId: string,
+    fecha?: string,
+  ): Promise<IPedidoGuardia[]>;
+  obtenerPedidosPaciente(
+    userId: string,
+    idPaciente: string,
+  ): Promise<IDetallePedidoGuardia[]>;
+  finalizarPedido(
+    userId: string,
+    idEstudio: string,
+    idPatient: string,
+  ): Promise<string>;
+  transferirPedido(userId: string, idEstudio: string): Promise<string>;
+  buscarDatosPacienteGuardia(
+    userId: string,
+    dni: string,
+  ): Promise<IDatosPacienteGuardia>;
 }

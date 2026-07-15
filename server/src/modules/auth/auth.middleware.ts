51ff7f39873ff42b7f4dd0dc1f314520ef8038f9
rodry <rodrymza@gmail.com>
2026-07-14 23:04:42 -0300

    refactor: eliminados comentarios en produccion

--- server/src/modules/auth/auth.middleware.ts
+++ server/src/modules/auth/auth.middleware.ts
@@ -1,28 +1,16 @@
 import { Request, Response, NextFunction } from "express";
-import jwt, { JwtPayload } from "jsonwebtoken";
-import { AppError } from "../../errors/AppError";
+import jwt from "jsonwebtoken";
+import { AppError } from "../../errors/AppError.js";
+import { IUserPayload } from "./auth.types.js";
 
-const SECRET = process.env.JWT_SECRET || "mi_secreto_super_seguro";
-
-interface IUsuarioPayload {
-  _id: string;
-  username: string;
-  nombre: string;
-  rol: "ADMIN" | "USER";
-  apellido: string;
-}
-
-// Extendemos Request para que TS no llore por 'req.user'
-export interface AuthRequest extends Request {
-  user?: IUsuarioPayload;
-}
+const SECRET = process.env.JWT_SECRET;
 
 export const protegerRuta = (
-  req: Request, // Usamos nuestra interfaz extendida
+  req: Request,
   res: Response,
   next: NextFunction,
 ) => {
-  const token = req.cookies.jwt; // Requiere 'cookie-parser' instalado
+  const token = req.cookies.jwt;
 
   if (!token) {
     return next(
@@ -31,22 +19,18 @@ export const protegerRuta = (
   }
 
   try {
-    const decoded = jwt.verify(token, SECRET!) as IUsuarioPayload;
-
-    // Guardamos los datos para el siguiente paso
-    req.user = decoded;
-
+    const decoded = jwt.verify(token, SECRET!) as IUserPayload;
+    req.user = decoded as any;
     next();
   } catch (error) {
     return next(new AppError("Token inválido o sesión expirada.", 401));
   }
 };
 
-// Middleware opcional para roles, implementar luego para diferentes roles
 export const restringirA = (...rolesPermitidos: string[]) => {
   return (req: Request, res: Response, next: NextFunction) => {
-    const user = req.user as JwtPayload;
-    if (!rolesPermitidos.includes(user.rol)) {
+    const user = req.user as IUserPayload;
+    if (!user || !rolesPermitidos.includes(user.rol)) {
       return next(
         new AppError("No tienes permisos para realizar esta acción", 403),
       );

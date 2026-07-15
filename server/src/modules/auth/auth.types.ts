3d2f026b7d8479b898463ee991cf0e4b497c79fb
rodry <rodrymza@gmail.com>
2026-07-14 23:05:30 -0300

    feat: omitida clave hsi en respuesta para frontend

--- server/src/modules/auth/auth.types.ts
+++ server/src/modules/auth/auth.types.ts
@@ -1,22 +1,20 @@
 export interface IUsuarioDB {
-  _id: string;
+  id: number;
   username: string;
   password?: string;
-  rol: "ADMIN" | "USER"; // Tipado estricto de roles
+  rol: "ADMIN" | "USER";
   nombre: string;
   apellido: string;
-  legajo?: string;
+  hsi_username?: string;
+  hsi_password?: string;
 }
 
-// Omitimos 'password' para asegurar que TS nos avise si intentamos enviarla
-export type IUsuarioResponse = Omit<IUsuarioDB, "password">;
+export type IUsuarioResponse = Omit<IUsuarioDB, "password" | "hsi_password">;
 
-// 3. Lo que guardamos dentro del JWT (Cookie)
 export interface IUserPayload {
-  id: string;
+  id: number;
   username: string;
   rol: string;
   nombre: string;
   apellido: string;
-  // Agrega aquí cualquier otro dato que necesites leer rápido en los controllers
 }

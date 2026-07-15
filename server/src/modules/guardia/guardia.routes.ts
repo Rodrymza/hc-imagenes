4760026ad94ba4ecb1e4a421afd344140c8a9cf7
rodry <rodrymza@gmail.com>
2026-07-14 23:06:34 -0300

    feat: añadida ruta logout

--- server/src/modules/guardia/guardia.routes.ts
+++ server/src/modules/guardia/guardia.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { guardiaControler } from "./guardia.controller";
+import { guardiaControler } from "./guardia.controller.js";
 
 const guardiaRoutes = Router();
 
 guardiaRoutes.post("/login", guardiaControler.loginGuardia);
+guardiaRoutes.post("/logout", guardiaControler.logoutGuardia);
 guardiaRoutes.get("/pedidos", guardiaControler.getPedidosGuardia);
 guardiaRoutes.get(
   "/paciente/:idPatient/pedidos",

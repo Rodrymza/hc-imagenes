86198701851c9e8ad0854dfbbd8f92f6b7cd18e3
rodry <rodrymza@gmail.com>
2026-07-21 09:03:41 -0300

    feat: agregadas rutas de logica de operador

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { authController } from "./auth.controller.js";
+import { authOperatorController } from "./auth.operator.controller.js";
 import { protegerRuta } from "./auth.middleware.js";
 
 const authRoutes = Router();
@@ -7,5 +8,16 @@ const authRoutes = Router();
 authRoutes.post("/login", authController.login);
 authRoutes.get("/logout", protegerRuta, authController.logout);
 authRoutes.get("/verify", protegerRuta, authController.verifyUser);
+
+authRoutes.post(
+  "/change-operator",
+  protegerRuta,
+  authOperatorController.changeOperator,
+);
+authRoutes.get(
+  "/active-operator",
+  protegerRuta,
+  authOperatorController.getActiveOperator,
+);
 
 export default authRoutes;
